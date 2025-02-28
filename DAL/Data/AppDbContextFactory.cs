d05e3b521347becc08442d041b086e1f1aadac72
Platon Matšinski <platonmatsinski@Platons-Air.lan>
2025-02-28 22:29:28 +0200

    Try to fix db first loading

--- DAL/Data/AppDbContextFactory.cs
+++ DAL/Data/AppDbContextFactory.cs
@@ -1,22 +1,55 @@
 using DAL.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-
-namespace DAL.Data;
+using Microsoft.Extensions.Configuration;
 
 public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
 {
     public AppDbContext CreateDbContext(string[] args)
     {
+        // Use config file
+        IConfigurationRoot configuration = null;
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
         
-        var connectionString = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development"
-            ? "Host=localhost;Database=bakesale;Username=postgres;Password=postgres"
-            : "Host=db;Database=bakesale;Username=postgres;Password=postgres";
+        try
+        {
+            // Try to find appsettings.json
+            string projectDir = Directory.GetCurrentDirectory();
             
-        optionsBuilder.UseNpgsql(connectionString
-        );
-
+            while (projectDir != null && !File.Exists(Path.Combine(projectDir, "appsettings.json")))
+            {
+                projectDir = Directory.GetParent(projectDir)?.FullName;
+            }
+            
+            if (projectDir != null)
+            {
+                configuration = new ConfigurationBuilder()
+                    .SetBasePath(projectDir)
+                    .AddJsonFile("appsettings.json", optional: false)
+                    .Build();
+                
+                var connString = configuration.GetConnectionString("LocalConnection");
+                if (!string.IsNullOrEmpty(connString))
+                {
+                    optionsBuilder.UseNpgsql(connString);
+                    return new AppDbContext(optionsBuilder.Options);
+                }
+            }
+        }
+        catch
+        {
+            
+        }
+        
+        string envConnString = Environment.GetEnvironmentVariable("DATABASE_URL");
+        if (!string.IsNullOrEmpty(envConnString))
+        {
+            optionsBuilder.UseNpgsql(envConnString);
+            return new AppDbContext(optionsBuilder.Options);
+        }
+        
+        // If nothing work, uses default connection string.
+        optionsBuilder.UseNpgsql("Host=localhost;Database=bakesale;Username=postgres;Password=postgres");
         return new AppDbContext(optionsBuilder.Options);
     }
 }
